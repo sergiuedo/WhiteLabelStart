d57b9de16880c43790f1a86461a47ee698f9dc08
Baraian Sergiu <sbaraian@the-mac.local>
2016-09-11 12:23:46 +0300

    initial commit

---
+++ WhiteLabel/WhiteLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhiteLabel
+//
+//  Created by Baraian Sergiu on 11/09/16.
+//  Copyright © 2016 Baraian Sergiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhiteLabel/WhiteLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhiteLabel
+//
+//  Created by Baraian Sergiu on 11/09/16.
+//  Copyright © 2016 Baraian Sergiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
